7a356c34a6fb7fbf1d5b9650141eaceca33968fb
rafika khatun <rafikakhatun607@gmail.com>
2025-07-25 00:58:00 +0530

    Added AuthProvider using Context API for global state management

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import { Outlet } from 'react-router-dom';
 import Footer from '../components/Footer';
+import Home from '../pages/home';
 
 const Layout = () => {
     return (
