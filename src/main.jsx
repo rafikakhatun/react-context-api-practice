f054f2b37cbd8cd0b345f19aa564a4130e65b9f1
rafika khatun <rafikakhatun607@gmail.com>
2025-07-24 22:29:46 +0530

    Initialize React Router and setup RouterProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,36 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+const router = createBrowserRouter([
+
+
+
+
+
+
+
+
+
+
+
+
+])
+
+
+
+
+
+
+
+
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
