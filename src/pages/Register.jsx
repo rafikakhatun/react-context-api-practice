2e85d8f605d6d9b68adf231ea5e7ad396ca03db6
rafika khatun <rafikakhatun607@gmail.com>
2025-07-27 22:30:19 +0530

    accessed register method from AuthContext using useContext hook

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import  { AuthContext } from '../provider/AuthProvider';
 
 const Register = () => {
+
+    const {register} = useContext(AuthContext);
+
+    console.log(register)
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+    }
+
+    
     
 
 
@@ -10,10 +22,11 @@ const Register = () => {
         <>
             <div className='w-full h-[400px] flex justify-center items-center'>
                 <form className='p-8 shadow-lg rounded-md border border-gray-300 w-full max-w-md space-y-6'>
-                    <div className='space-y-2'>
+                    <div onChange={handleSubmit} className='space-y-2'>
                         <label className='block text-lg text-gray-800'>Email</label>
                         <input className=' w-full px-4 py-3 rounded-md outline-none border border-gray-300 bg-gray-200' type="email" placeholder='Enter a valid Email' name="email" />
                     </div>
+
                     <div className='space-y-2'>
                         <label className='block text-lg text-gray-800'>Password</label>
                         <input className='w-full px-4 py-3 rounded-md outline-none border border-gray-300 bg-gray-200' type="password" name="password" />
