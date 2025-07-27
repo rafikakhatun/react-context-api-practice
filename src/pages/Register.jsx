0a124f76df255a7cf51b48aca0a9e1522a231095
rafika khatun <rafikakhatun607@gmail.com>
2025-07-27 22:15:33 +0530

    Create Register component with email & password input form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
 
 const Register = () => {
-    return (
-        <div>
+    
 
-            <form>
-                <div>
-                    <label>Email</label>
-                    <input type="email" placeholder='Enter a valid Email' name="email" />
-                </div>
-                <div>
-                    <label>Password</label>
-                    <input type="password" name="password" />
-                </div>
-            </form>
-            
-        </div>
+
+
+
+    return (
+        <>
+            <div className='w-full h-[400px] flex justify-center items-center'>
+                <form className='p-8 shadow-lg rounded-md border border-gray-300 w-full max-w-md space-y-6'>
+                    <div className='space-y-2'>
+                        <label className='block text-lg text-gray-800'>Email</label>
+                        <input className=' w-full px-4 py-3 rounded-md outline-none border border-gray-300 bg-gray-200' type="email" placeholder='Enter a valid Email' name="email" />
+                    </div>
+                    <div className='space-y-2'>
+                        <label className='block text-lg text-gray-800'>Password</label>
+                        <input className='w-full px-4 py-3 rounded-md outline-none border border-gray-300 bg-gray-200' type="password" name="password" />
+                    </div>
+
+                    <button className='w-full px-4 py-3 rounded-md bg-blue-600 text-white hover:bg-blue-700 cursor-pointer'>Register</button>
+
+                </form>
+
+            </div>
+        </>
     );
 };
 
